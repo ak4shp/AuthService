880c3b3318ff6e3d1f88fe299070190a05be19ab
ak4shp <akashpatelp83@gmail.com>
2024-11-14 05:35:50 +0530

    Added new webapi project

---
+++ AuthService/app.auth/Controllers/AuthController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace app.auth.Controllers
+{
+
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : Controller
+    {
+
+        private readonly ILogger<AuthController> logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        [Route("sign-up")]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [Route("sign-in")]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [Route("revoke-token")]
+        public IActionResult RevokeTokan()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [Route("renew-token")]
+        public IActionResult RenewToken()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        [Route("get-item")]
+        public IActionResult GetItem()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [Route("save-item")]
+        public IActionResult SaveItem()
+        {
+            return View();
+        }
+    }
+}
